cbd75cb80ec7335330bb2ec0cb34db0c6f0fbc34
Shady Mohammed <sa187@smail.ucas.edu.ps>
2026-03-23 17:53:39 +0200

    update2

--- components/footer.tsx
+++ components/footer.tsx
@@ -24,7 +24,7 @@ const socialLinks = [
   icon: () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"  // 👈 مهم جدًا
+      viewBox="0 0 24 24"  
       className="w-5 h-5"
       fill="currentColor"
     >
